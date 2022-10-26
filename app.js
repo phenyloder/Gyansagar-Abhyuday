7196dcb53fec7d884990e5b000065656a457bf69
Vinayak Das <vinayakd909@gmail.com>
2022-10-26 19:52:37 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req,res)=>{
+    res.render("index");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to the port ${PORT}`);
+})
