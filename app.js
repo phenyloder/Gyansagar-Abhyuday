038a43b542d8d4690e2f78347d85f418be402842
Vinayak Das <vinayakd909@gmail.com>
2022-10-29 13:01:42 +0530

    UPD: Port Address

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
